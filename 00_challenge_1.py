1064a4f2f307dacba97ad72d5b42fd7bbdbe2939
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-13 09:42:50 -0500

    repository created

---
+++ 00_challenge_1.py
@@ -0,0 +1,11 @@
+"""El área de un triángulo se describe de la siguiente manera: A = (b * h) / 2 .
+
+Escribe un programa que tome la base y la altura como parámetros y calcule el área del triángulo.
+
+**Bonus: el programa debe determinar si el triángulo es isósceles, equilátero o escaleno**
+"""
+
+base = 1
+height = 2
+area = (base * height) / 2
+
